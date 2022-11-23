529e520e9aa7872efb400b94b8c5aa77c28bf7cd
ariza1992 <ariza1992@hotmail.com>
2022-11-22 19:39:17 -0500

    segundo envio, rutas de administracion

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarPersonaComponent } from './personas/buscar-persona/buscar-persona.component';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
+import { EliminarPersonaComponent } from './personas/eliminar-persona/eliminar-persona.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "crear-persona",
+    component: CrearPersonaComponent
+  },
+  {
+    path: "buscar-persona",
+    component: BuscarPersonaComponent
+  },
+  {
+    path: "editar-persona",
+    component: EditarPersonaComponent
+  },
+  {
+    path: "eliminar-persona",
+    component: EliminarPersonaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
